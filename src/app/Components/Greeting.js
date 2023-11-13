1faf8ca57251e6439397bb6e33742843cd6ac91f
Robin <robinkelsick@gmail.com>
2023-11-13 10:58:42 -0500

    playerName sets the player's name and adds it to local storage

--- src/app/Components/Greeting.js
+++ src/app/Components/Greeting.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Greeting = () => {
+  const [playerName, setPlayerName] = useState('');
+
+  const handleNameChange = (e) => {
+    setPlayerName(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    localStorage.setItem('playerName', playerName);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen'>
       <div className='w-3/4 h-[600px] flex justify-center items-center'>
@@ -8,12 +19,20 @@ const Greeting = () => {
           <h1 className='text-2xl text-secondary'>
             Hi there! What do we call you?
           </h1>
+          <label htmlFor='playerName' className='sr-only'>
+            Your Name
+          </label>
           <input
+            id='playerName'
             type='text'
             placeholder='Your Name'
+            value={playerName}
+            onChange={handleNameChange}
             className='w-full max-w-xs my-6 shadow-sm input input-bordered'
-          />
-          <button className='shadow-md btn btn-accent'>Let's play</button>
+          />{' '}
+          <form onSubmit={handleSubmit}>
+            <button className='shadow-md btn btn-accent'>Let's play</button>
+          </form>
         </div>
       </div>
     </div>
